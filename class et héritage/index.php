46f799ba28dca45f6bbd59d3c090f3379544c8e9
Fabrice DUJARDIN <44850811+fabricedujardinportfolio@users.noreply.github.com>
2021-04-26 10:56:55 +1100

    correction espace

--- class et héritage/index.php
+++ class et héritage/index.php
@@ -9,6 +9,7 @@
 <body>
     
 <?php
+
 class Dog {
   // Properties
   public $name;
@@ -44,61 +45,66 @@ class Dog {
 }
 class cat {
     // Properties
-    public $name;
-    public $color;
-    public $tail;
-    public $breeds;
+  public $name;
+  public $color;
+  public $tail;
+  public $breeds;
   
     // Methods
-    function set_name($name) {
-      $this->name = $name;
-    }
-    function get_name() {
-      return $this->name;
-    }
-    function set_color($color) {
-      $this->color = $color;
-    }
-    function get_color() {
-      return $this->color;
-    }
-    function set_tail($tail) {
-      $this->tail = $tail;
-    }
-    function get_tail() {
-      return $this->tail;
-    }
-    function set_breeds($breeds) {
-      $this->breeds = $breeds;
-    }
-    function get_breeds() {
-      return $this->breeds;
-    }
+  function set_name($name) {
+    $this->name = $name;
   }
-  $cat1 = new cat();
-  $cat1->set_name('bobyCat');
-  $cat1->set_color('blue');
-  $cat1->set_tail('2m');
-  $cat1->set_breeds('persan');
-  echo "CAT";
-  echo "<br>";
-  echo "Name: " . $cat1->get_name();
-  echo "<br>";
-  echo "Color: " .  $cat1->get_color();
-  echo "<br>";
-  echo "tail: " . $cat1->get_tail();
-  echo "<br>";
-  echo "breeds: " .  $cat1->get_breeds();
-  echo "<br>";
-  echo "<br>";
-  echo "<br>";
-  echo "DOG";
-  echo "<br>";
+  function get_name() {
+    return $this->name;
+  }
+  function set_color($color) {
+    $this->color = $color;
+  }
+  function get_color() {
+    return $this->color;
+  }
+  function set_tail($tail) {
+    $this->tail = $tail;
+  }
+  function get_tail() {
+    return $this->tail;
+  }
+  function set_breeds($breeds) {
+    $this->breeds = $breeds;
+  }
+  function get_breeds() {
+    return $this->breeds;
+  }
+}
+
+$cat1 = new cat();
+$cat1->set_name('bobyCat');
+$cat1->set_color('blue');
+$cat1->set_tail('2m');
+$cat1->set_breeds('persan');
+
+echo "CAT";
+echo "<br>";
+echo "Name: " . $cat1->get_name();
+echo "<br>";
+echo "Color: " .  $cat1->get_color();
+echo "<br>";
+echo "tail: " . $cat1->get_tail();
+echo "<br>";
+echo "breeds: " .  $cat1->get_breeds();
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "DOG";
+echo "<br>";
+
 $dog1 = new Dog();
+
 $dog1->set_name('boby');
 $dog1->set_color('blue');
 $dog1->set_tail('2m');
 $dog1->set_breeds('labrador');
+
 echo "Name: " . $dog1->get_name();
 echo "<br>";
 echo "Color: " .  $dog1->get_color();
